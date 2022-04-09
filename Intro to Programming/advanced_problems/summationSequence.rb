7ff7cb4e432c7dee87a50e33b482c3c29670d6cb
Augusto <aleal4@gmail.com>
2022-04-08 18:50:19 -0700

    fibonnaci sequence

--- Intro to Programming/advanced_problems/summationSequence.rb
+++ Intro to Programming/advanced_problems/summationSequence.rb
@@ -1,7 +1,7 @@
 # A number's summation is the sum of all positive numbers less than or equal to the number. For example: the summation of 3 is 6 because 1 + 2 + 3 = 6, the summation of 6 is 21 because 1 + 2 + 3 + 4 + 5 + 6 = 21. Write a method summation_sequence that takes in a two numbers: start and length. The method should return an array containing length total elements. The first number of the sequence should be the start number. At any point, to generate the next element of the sequence we take the summation of the previous element. You can assume that length is not zero.
 
 def summation(num) 
-  sum= 0
+  sum  = 0
 
   (1..num).each do |i|
     sum += i
