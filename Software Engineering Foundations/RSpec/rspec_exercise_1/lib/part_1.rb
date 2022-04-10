af3018b098ca6c98a891ac9917b141cbeda5b567
Augusto <aleal4@gmail.com>
2022-04-09 21:04:05 -0700

    rspec part1

---
+++ Software Engineering Foundations/RSpec/rspec_exercise_1/lib/part_1.rb
@@ -0,0 +1,34 @@
+def average(num1, num2)
+  average = (num1 + num2) /2.0
+  return average
+end
+
+def average_array(array)
+  array.sum / (array.length * 1.0)
+end
+
+def repeat(str, num)
+  str * num
+#   new_str = ""
+#   num.times { new_str += str}
+#   new_str
+end
+
+def yell(str)
+  str.upcase + '!'
+end
+
+def alternating_case(sentence)
+  words = sentence.split(" ")
+
+  new_words = words.map.with_index do |word, i|
+    if i % 2 == 0
+      word.upcase
+    else
+      word.downcase
+    end
+  end
+
+  new_words.join(" ")
+end
+
