892e7224afa257dfd5c06da480dd535dae06d1b2
Augusto <aleal4@gmail.com>
2022-04-16 20:00:31 -0700

    greater-proc-value .exit

--- Software Engineering Foundations/Blocks_Procs/procs_project/lib/part_2.rb
+++ Software Engineering Foundations/Blocks_Procs/procs_project/lib/part_2.rb
@@ -13,3 +13,15 @@ def word_changer(string, &prc)
 
   new_words.join(' ')
 end
+
+def greater_proc_value(num, prc_1, prc_2)
+  
+  num_1 = prc_1.call(num)
+  num_2 = prc_2.call(num)
+
+  if num_1 > num_2
+    return num_1
+  else
+    return num_2
+  end
+end
