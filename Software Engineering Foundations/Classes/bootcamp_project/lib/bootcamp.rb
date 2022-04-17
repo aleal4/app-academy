12a466316df45bfc706f11580dfe1805f4f82408
Augusto <aleal4@gmail.com>
2022-04-17 10:39:02 -0700

    trial

--- Software Engineering Foundations/Classes/bootcamp_project/lib/bootcamp.rb
+++ Software Engineering Foundations/Classes/bootcamp_project/lib/bootcamp.rb
@@ -7,4 +7,37 @@ class Bootcamp
     @students = Array.new
     @grades = Hash.new { |h,k| h[k] = []}
   end
+
+  def name
+    @name
+  end
+
+  def slogan
+    @slogan
+  end
+
+  def teachers
+    @teachers
+  end
+
+  def students 
+    @students
+  end
+
+  def hire=(new_hire)
+    @teachers = @teachers << new_hire
+  end
+
+  def enroll=(student)
+    if students.length < student_capacity
+      students << student
+      return true
+    else
+      return false
+    end
+  end
+
+  def enrolled?(student)
+    students.include?(student)
+  end
 end
