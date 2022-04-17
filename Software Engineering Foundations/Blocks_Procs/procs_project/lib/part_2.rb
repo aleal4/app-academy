c2d2606fd3d739aecd21bb68a647cb517fe19768
Augusto <aleal4@gmail.com>
2022-04-16 19:43:48 -0700

    word changer

--- Software Engineering Foundations/Blocks_Procs/procs_project/lib/part_2.rb
+++ Software Engineering Foundations/Blocks_Procs/procs_project/lib/part_2.rb
@@ -1,3 +1,15 @@
 def reverser(string, &prc)
+  # reverse string first, then give it into the block
   prc.call(string.reverse)  
+end
+
+def word_changer(string, &prc)
+  words = string.split(' ')
+  new_words = []
+
+  words.each do |word|
+    new_words << prc.call(word)
+  end
+
+  new_words.join(' ')
 end
