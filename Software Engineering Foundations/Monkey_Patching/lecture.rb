e35cfa2b61545bdda2e7721c69e156d41f11eb78
Augusto <aleal4@gmail.com>
2022-04-17 11:49:07 -0700

    array monkeypatching

--- Software Engineering Foundations/Monkey_Patching/lecture.rb
+++ Software Engineering Foundations/Monkey_Patching/lecture.rb
@@ -7,20 +7,29 @@
 # p 'hello'.upcase?
 # p 'HELLO'.upcase?
 
-class Integer 
-  def prime? 
-    return fasle if self < 2
+# class Integer 
+#   def prime? 
+#     return fasle if self < 2
       
-    (2...self).each do |i|
-      if self % i == 0
-        return false
-      end
-    end
+#     (2...self).each do |i|
+#       if self % i == 0
+#         return false
+#       end
+#     end
 
-    return true
+#     return true
+#   end
+# end
+
+# p 7.prime?
+# p 11.prime?
+# p 12.prime?
+
+class Array
+  def has_zero?
+    self.include?(0)
   end
 end
 
-p 7.prime?
-p 11.prime?
-p 12.prime?
+p [4,2,3,0,'hello'].has_zero? 
+p [4,2,3,'hello'].has_zero? 
