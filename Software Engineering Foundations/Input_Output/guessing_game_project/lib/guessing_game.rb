053fbd6657a64e52899a34102ae7b43e405f66da
Augusto <aleal4@gmail.com>
2022-04-17 16:42:15 -0700

    up to asking user

--- Software Engineering Foundations/Input_Output/guessing_game_project/lib/guessing_game.rb
+++ Software Engineering Foundations/Input_Output/guessing_game_project/lib/guessing_game.rb
@@ -1,9 +1,34 @@
 class GuessingGame
-  def initialize(min, max, num_attemps = 0)
-    @max = max
-    @min = min
-    @secret_num = rand(@min..@max)
+  def initialize(min, max)
+    @secret_num = rand(min..max)
+    @num_attempts = 0
     @game_over = false
   end
+
+  def num_attempts
+    @num_attempts
+  end
+
+  def game_over?
+    @game_over 
+  end
+
+  def check_num(value)
+    @num_attempts += 1
+    if value == @secret_num
+      @game_over = true
+      p 'you win'
+    elsif value > @secret_num
+      p 'too big'
+    else 
+      p 'too small'
+    end
+  end
+
+  def ask_user
+    
+  end
+
+
 
 end
