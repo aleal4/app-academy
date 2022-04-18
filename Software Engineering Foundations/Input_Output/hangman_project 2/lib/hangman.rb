a642063c7e1c0ce26cc8abeb2139667c0656a79d
Augusto <aleal4@gmail.com>
2022-04-17 18:23:27 -0700

    commit

--- Software Engineering Foundations/Input_Output/hangman_project 2/lib/hangman.rb
+++ Software Engineering Foundations/Input_Output/hangman_project 2/lib/hangman.rb
@@ -1,4 +1,12 @@
 class Hangman
   DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
 
+  def self.random_word
+    DICTIONARY.sample 
+  end
+
+  def initialize 
+    @secret_word = Hangman.random_word
+    @guess_word = Array new(@secret_word.length, '_')
+  end
 end
