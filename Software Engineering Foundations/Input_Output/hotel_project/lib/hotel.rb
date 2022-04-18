6698bd13c9088b48b744475bfa3671733020ad1c
Augusto <aleal4@gmail.com>
2022-04-18 09:34:23 -0700

    hotel initializing

--- Software Engineering Foundations/Input_Output/hotel_project/lib/hotel.rb
+++ Software Engineering Foundations/Input_Output/hotel_project/lib/hotel.rb
@@ -1,5 +1,17 @@
 require_relative "room"
 
 class Hotel
+
+  def initialize(name, hash)
+    @name = name
+    @rooms = {}
+    hash = hash.each do |room_name, capacity|
+      @rooms[room_name] = Room.new(capacity)
+    end
+  end
+
+
+
   
+
 end
