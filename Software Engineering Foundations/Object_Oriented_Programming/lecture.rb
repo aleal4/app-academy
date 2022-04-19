c7517f5924a46a2a342c20fa68f6cc223ab33ca4
Augusto <aleal4@gmail.com>
2022-04-18 17:18:02 -0700

    imported new project files

--- Software Engineering Foundations/Object_Oriented_Programming/lecture.rb
+++ Software Engineering Foundations/Object_Oriented_Programming/lecture.rb
@@ -1,26 +1,36 @@
 class Dog
+
+  # attr_reader defines our getter methods
+  # attr_reader :name, :age
+  
+  #attr_writer defines our setter methods
+  # attr_writer :name, :age
+  
+  #attr_accessor defines both getters and setters 
+  attr_accessor :name, :age
+
+
   def initialize(name, age, favorite_food)
     @name = name
     @age = age
     @favorite_food = favorite_food
   end
 
-  def name
-    @name
-  end
+  # def name=(new_name)
+  #   @name = new_name
+  # end
 
-  def age
-    @age
-  end
-
-  def name=(new_name)
-    @name = new_name
-  end
-
-  def age=(new_age)
-    @age = new_age
-  end
+  # def age=(new_age)
+  #   @age = new_age
+  # end
 end
 
 my_dog = Dog.new("Fido", 2, "pizza")
-p my_dog.name
+
+
+p my_dog.age
+my_dog.age = 3
+p my_dog.age
+
+# my_dog.name = 'Spot'
+# p my_dog.name
