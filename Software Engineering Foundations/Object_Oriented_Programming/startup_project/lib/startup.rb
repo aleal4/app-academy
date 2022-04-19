7e86127056a04c362b0f432a792a3a6e8eb37877
Augusto <aleal4@gmail.com>
2022-04-18 21:13:33 -0700

    startup classes

--- Software Engineering Foundations/Object_Oriented_Programming/startup_project/lib/startup.rb
+++ Software Engineering Foundations/Object_Oriented_Programming/startup_project/lib/startup.rb
@@ -1,5 +1,10 @@
 require "employee"
 
 class Startup
+  attr_reader :employees
+  attr_accessor :name, :funding, :salaries
 
+  def initialize(name, funding, salaries)
+    @employees = []
+  end
 end
