fa4ff93be81a53649e971c524b27135cf1bbd0a7
Augusto <aleal4@gmail.com>
2022-04-20 00:38:56 -0700

    more work

--- Software Engineering Foundations/Object_Oriented_Programming/mastermind_project/lib/code.rb
+++ Software Engineering Foundations/Object_Oriented_Programming/mastermind_project/lib/code.rb
@@ -6,18 +6,32 @@ class Code
     "Y" => :yellow
   }
 
+  attr_reader :pegs 
+
   def self.valid_pegs?(chars)
     chars.all? { |chars| POSSIBLE_PEGS.has_key?(chars.upcase)}
   end
 
-  attr_reader :pegs 
-  
+  def self.random(length)
+    random_pegs = []
+    length.times { random_pegs << POSSIBLE_PEGS.keys.sample }
+    Code.new(random_pegs)
+  end
+
+
   def initialize(chars)
     if Code.valid_pegs?(chars)
       @pegs = chars.map(&:upcase) 
     else
       raise 'error!'
     end
+  end
 
+  def [](index)
+    @pegs[index]
+  end
+
+  def length
+    @pegs.length
   end
 end
