24997655539c206d8ab4312a8e998bb6e86b347e
Augusto <aleal4@gmail.com>
2022-04-21 21:10:44 -0700

    print_grid

--- Software Engineering Foundations/Object_Oriented_Programming/battleship_project/lib/board.rb
+++ Software Engineering Foundations/Object_Oriented_Programming/battleship_project/lib/board.rb
@@ -1,4 +1,11 @@
 class Board
+  
+  def self.print_grid(arr)
+    arr.each do |row| 
+      puts row.join(" ")
+    end
+  end
+
   def initialize(n)
     @grid = Array.new(n) {Array.new(n, :N)}
     @size = n * n
