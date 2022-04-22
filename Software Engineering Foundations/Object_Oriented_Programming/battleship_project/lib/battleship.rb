3f1a9293042fcd189575aef797881184ff171af7
Augusto <aleal4@gmail.com>
2022-04-21 21:39:30 -0700

    game started

--- Software Engineering Foundations/Object_Oriented_Programming/battleship_project/lib/battleship.rb
+++ Software Engineering Foundations/Object_Oriented_Programming/battleship_project/lib/battleship.rb
@@ -2,10 +2,19 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
+
+  attr_reader :board, :player
+
   def initialize(n)
     @player = Player.new
     @board = Board.new(n)
     @remaining_misses = @board.size / 2.0
+  end
+
+  def start_game
+    @board.place_random_ships
+    puts @board.num_ships
+    @board.print
   end
 
 end
