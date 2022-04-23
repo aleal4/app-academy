6ddf39dc6d6424fc8e95b427ab30ebf70ba3d25e
Augusto <aleal4@gmail.com>
2022-04-23 12:40:23 -0700

    bubble sort

--- Software Engineering Foundations/Foundations_Recap/spaceship_lecture.rb
+++ Software Engineering Foundations/Foundations_Recap/spaceship_lecture.rb
@@ -8,6 +8,23 @@ def compare(a, b)
   end
 end
 
-compare(-3, 10)
-compare(-4.5, -5)
-compare(12, 12)
+# compare(-3, 10)
+# compare(-4.5, -5)
+# compare(12, 12)
+
+def bubble_sort(array)
+  sorted = false
+  while !sorted
+    sorted = true
+
+    (0...array.length - 1).each do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        sorted = false
+      end
+    end
+  end
+  array
+end
+
+p bubble_sort([6, 10, -5, 9, 0 , 8, 3, 2])
