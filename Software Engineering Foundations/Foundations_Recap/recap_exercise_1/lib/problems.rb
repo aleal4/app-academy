f660fabc732ea6267361343e150ffc549304163d
Augusto <aleal4@gmail.com>
2022-04-23 14:43:11 -0700

    hash problem

--- Software Engineering Foundations/Foundations_Recap/recap_exercise_1/lib/problems.rb
+++ Software Engineering Foundations/Foundations_Recap/recap_exercise_1/lib/problems.rb
@@ -63,7 +63,13 @@ class Hash
     # hash_2.my_select { |k, v| k + 1 == v }      # => {10=>11, 5=>6, 7=>8})
     # hash_2.my_select                            # => {4=>4}
     def my_select(&prc)
+        prc ||= Proc.new{|k, v| k == v}
 
+        new_hash = {}
+        self.each do |k, v| 
+            new_hash[k] = v if prc.call(k, v)
+        end
+        new_hash
     end
 end
 
