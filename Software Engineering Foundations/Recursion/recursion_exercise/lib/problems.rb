21478ca21e3c4d2c19e50bfc12c9743dcae3188e
Augusto <aleal4@gmail.com>
2022-04-22 22:49:18 -0700

    recursive roll

--- Software Engineering Foundations/Recursion/recursion_exercise/lib/problems.rb
+++ Software Engineering Foundations/Recursion/recursion_exercise/lib/problems.rb
@@ -54,7 +54,10 @@ end
 # sum_array([5, 2])         # => 7
 # sum_array([4, 10, -1, 2]) # => 15
 def sum_array(array)
-
+  return 0 if array.empty?
+  first = array[0]
+  rest = array[1..-1]
+  first + sum_array(rest)
 end
 
 
