754a14e54ec1d53ad46c37e92bfab7b31a2569fd
Augusto <aleal4@gmail.com>
2022-04-23 20:06:53 -0700

    no consecutive repeats

--- Software Engineering Foundations/Foundations_Recap/general_problems.rb
+++ Software Engineering Foundations/Foundations_Recap/general_problems.rb
@@ -8,6 +8,19 @@ def no_dupes?(array)
   end
 end
 
-p no_dupes?([1, 1, 2, 1, 3, 2, 4])         # => [3, 4]
-p no_dupes?(['x', 'x', 'y', 'z', 'z'])     # => ['y']
-p no_dupes?([true, true, true])          
+# p no_dupes?([1, 1, 2, 1, 3, 2, 4])         # => [3, 4]
+# p no_dupes?(['x', 'x', 'y', 'z', 'z'])     # => ['y']
+# p no_dupes?([true, true, true])          
+
+def no_consecutive_repeats?(arr)
+  (0...arr.length - 1).each do |i|
+    return false if arr[i] == arr[i + 1]
+  end
+  true
+end
+
+p no_consecutive_repeats?(['cat', 'dog', 'mouse', 'dog'])     # => true
+p no_consecutive_repeats?(['cat', 'dog', 'dog', 'mouse'])     # => false
+p no_consecutive_repeats?([10, 42, 3, 7, 10, 3])              # => true
+p no_consecutive_repeats?([10, 42, 3, 3, 10, 3])              # => false
+p no_consecutive_repeats?(['x'])                              # => true
