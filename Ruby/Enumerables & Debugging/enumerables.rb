813cb27154fcbaf63261a1bf698dfe72b1329c4f
Augusto <aleal4@gmail.com>
2022-04-24 15:54:23 -0700

    my_all?

--- Ruby/Enumerables & Debugging/enumerables.rb
+++ Ruby/Enumerables & Debugging/enumerables.rb
@@ -28,6 +28,20 @@ class Array
 
     selected
   end
+
+  def my_any?(&prc)
+    self.my_each do |ele|
+      return true if prc.call(ele) 
+    end
+    false
+  end
+
+  def my_all?(&prc)
+    self.my_each do |ele|
+      return false unless prc.call(ele)
+    end
+    true
+  end
 end
 
 
@@ -49,6 +63,12 @@ end
 # p a.my_select { |num| num > 1 } # => [2, 3]
 # p a.my_select { |num| num == 4 } # => []
 
+# a = [1, 2, 3]
+# p a.my_reject { |num| num > 1 } # => [1]
+# p a.my_reject { |num| num == 4 } # => [1, 2, 3]
+
 a = [1, 2, 3]
-p a.my_reject { |num| num > 1 } # => [1]
-p a.my_reject { |num| num == 4 } # => [1, 2, 3]
+p a.my_any? { |num| num > 1 } # => true
+p a.my_any? { |num| num == 4 } # => false
+p a.my_all? { |num| num > 1 } # => false
+p a.my_all? { |num| num < 4 } # => true
