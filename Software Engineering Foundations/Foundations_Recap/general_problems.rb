2d120627bc4a12f8b2965ce78ed3f296d1171927
Augusto <aleal4@gmail.com>
2022-04-24 11:03:24 -0700

    lucas sequence recursion problem

--- Software Engineering Foundations/Foundations_Recap/general_problems.rb
+++ Software Engineering Foundations/Foundations_Recap/general_problems.rb
@@ -183,17 +183,35 @@ end
 def multiply(a, b)
   return 0 if b == 0
 
-  if b < 0
+  if b < 
     -(a + multiply(a, (-b) - 1))
   else
     a + multiply(a, b - 1)
   end
 end
 
-p multiply(3, 5)        # => 15
-p multiply(5, 3)        # => 15
-p multiply(2, 4)        # => 8
-p multiply(0, 10)       # => 0
-p multiply(-3, -6)      # => 18
-p multiply(3, -6)       # => -18
-p multiply(-3, 6) 
+# p multiply(3, 5)        # => 15
+# p multiply(5, 3)        # => 15
+# p multiply(2, 4)        # => 8
+# p multiply(0, 10)       # => 0
+# p multiply(-3, -6)      # => 18
+# p multiply(3, -6)       # => -18
+# p multiply(-3, 6) 
+
+def lucas_sequence(length)
+  return [] if length == 0
+  return [2] if length == 1
+  return [2, 1] if length == 2
+
+  seq = lucas_sequence(length - 1) 
+  next_el = seq[-1] + seq[-2]
+  seq << next_el
+  seq
+end
+
+p lucas_sequence(0)   # => []
+p lucas_sequence(1)   # => [2]    
+p lucas_sequence(2)   # => [2, 1]
+p lucas_sequence(3)   # => [2, 1, 3]
+p lucas_sequence(6)   # => [2, 1, 3, 4, 7, 11]
+p lucas_sequence(8)
