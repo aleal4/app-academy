b519176b165a6caed96360bc8d54781d0b01b003
Augusto <aleal4@gmail.com>
2022-04-24 13:51:29 -0700

    debugging md

---
+++ Ruby/Enumerables & Debugging/enumerables.rb
@@ -0,0 +1,26 @@
+class Array 
+  def my_each(&prc)
+    i = 0
+    while i < self.length
+      prc.call(self[i])
+      i += 1
+    end
+    self
+  end
+
+end
+
+
+
+
+
+
+
+puts "My Each"
+# My Each
+# calls my_each twice on the array, printing all the numbers twice.
+return_value = [1, 2, 3].my_each do |num|
+  puts num
+end.my_each do |num|
+  puts num
+end
