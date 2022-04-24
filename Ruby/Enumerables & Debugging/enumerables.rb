309d867cfdf345f07f0cd035201918153e34b3f8
Augusto <aleal4@gmail.com>
2022-04-24 16:24:16 -0700

    enumerables zipper

--- Ruby/Enumerables & Debugging/enumerables.rb
+++ Ruby/Enumerables & Debugging/enumerables.rb
@@ -1,4 +1,4 @@
-# require 'byebug'
+require 'byebug'
 
 class Array 
   def my_each(&prc)
@@ -58,7 +58,8 @@ class Array
 
   def my_zip(*arrays)
     zipped = []
-
+    
+    debugger
     self.length.times do |i|
       sub_zip = [self[i]]
 
@@ -70,8 +71,6 @@ class Array
     end
 
     zipped  
-  end
-
   end
 
 
@@ -107,3 +106,13 @@ end
 # p a.my_all? { |num| num < 4 } # => true
 
 # p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+
+a = [ 4, 5, 6 ]
+b = [ 7, 8, 9 ]
+p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+# a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+# [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+
+# c = [10, 11, 12]
+# d = [13, 14, 15]
+# [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
