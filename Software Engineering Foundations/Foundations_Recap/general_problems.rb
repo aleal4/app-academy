34861bc1f17fdd5264a97f328e0a02a4b4fb8b72
Augusto <aleal4@gmail.com>
2022-04-23 20:11:09 -0700

    char indices

--- Software Engineering Foundations/Foundations_Recap/general_problems.rb
+++ Software Engineering Foundations/Foundations_Recap/general_problems.rb
@@ -19,8 +19,19 @@ def no_consecutive_repeats?(arr)
   true
 end
 
-p no_consecutive_repeats?(['cat', 'dog', 'mouse', 'dog'])     # => true
-p no_consecutive_repeats?(['cat', 'dog', 'dog', 'mouse'])     # => false
-p no_consecutive_repeats?([10, 42, 3, 7, 10, 3])              # => true
-p no_consecutive_repeats?([10, 42, 3, 3, 10, 3])              # => false
-p no_consecutive_repeats?(['x'])                              # => true
+# p no_consecutive_repeats?(['cat', 'dog', 'mouse', 'dog'])     # => true
+# p no_consecutive_repeats?(['cat', 'dog', 'dog', 'mouse'])     # => false
+# p no_consecutive_repeats?([10, 42, 3, 7, 10, 3])              # => true
+# p no_consecutive_repeats?([10, 42, 3, 3, 10, 3])              # => false
+# p no_consecutive_repeats?(['x'])                              # => true
+
+def char_indices(str)
+  hash = Hash.new{ |h, k| h[k] = []}
+  str.each_char.with_index do |char, i|
+    hash[char] << i
+  end
+  hash
+end
+
+p char_indices('mississippi')   # => {"m"=>[0], "i"=>[1, 4, 7, 10], "s"=>[2, 3, 5, 6], "p"=>[8, 9]}
+p char_indices('classroom')     # => {"c"=>[0], "l"=>[1], "a"=>[2], "s"=>[3, 4], "r"=>[5], "o"=>[6, 7], "m"=>[8]}
