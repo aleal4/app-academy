e7667d6149e727f1716aa37f18501629be25a256
Augusto <aleal4@gmail.com>
2022-04-23 20:55:55 -0700

    longest streak

--- Software Engineering Foundations/Foundations_Recap/general_problems.rb
+++ Software Engineering Foundations/Foundations_Recap/general_problems.rb
@@ -1,6 +1,6 @@
 def no_dupes?(array)
   count = Hash.new(0)
-  array.each do |ele|
+  arr ay.each do |ele|
     count[ele] += 1
   end
   count.keys.select do |key| 
@@ -26,12 +26,35 @@ end
 # p no_consecutive_repeats?(['x'])                              # => true
 
 def char_indices(str)
-  hash = Hash.new{ |h, k| h[k] = []}
+  indices = Hash.new{ |h, k| h[k] = []}
   str.each_char.with_index do |char, i|
-    hash[char] << i
+    indices[char] << i
   end
-  hash
+  indices
 end
 
-p char_indices('mississippi')   # => {"m"=>[0], "i"=>[1, 4, 7, 10], "s"=>[2, 3, 5, 6], "p"=>[8, 9]}
-p char_indices('classroom')     # => {"c"=>[0], "l"=>[1], "a"=>[2], "s"=>[3, 4], "r"=>[5], "o"=>[6, 7], "m"=>[8]}
+# p char_indices('mississippi')   # => {"m"=>[0], "i"=>[1, 4, 7, 10], "s"=>[2, 3, 5, 6], "p"=>[8, 9]}
+# p char_indices('classroom')     # => {"c"=>[0], "l"=>[1], "a"=>[2], "s"=>[3, 4], "r"=>[5], "o"=>[6, 7], "m"=>[8]}
+
+def longest_streak(str)
+  current_str = ''
+  best_str = ''
+
+  (0...str.length).each do |idx|
+    if str[idx] == str[idx + 1] || idx == 0
+      current_str += str[idx]
+    else
+      current_str = str[idx]
+    end
+  
+  best_str = current_str if current_str.length >= best_str.length
+  end
+  best_str
+
+end
+
+p longest_streak('a')           # => 'a'
+p longest_streak('abc')         # => 'c'
+p longest_streak('accccbbb')    # => 'cccc'
+p longest_streak('aaaxyyyyyzz') # => 'yyyyy
+p longest_streak('aaabbb')      # => 'bbb'
