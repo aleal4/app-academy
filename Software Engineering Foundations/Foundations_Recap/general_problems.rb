0e3ab85c89828415191354038a64f943fc01273b
Augusto <aleal4@gmail.com>
2022-04-24 11:55:50 -0700

    prime factorization

--- Software Engineering Foundations/Foundations_Recap/general_problems.rb
+++ Software Engineering Foundations/Foundations_Recap/general_problems.rb
@@ -217,10 +217,20 @@ end
 
 def prime_factorization(num)
 
-  (2..num).each do |factor|
+  (2...num).each do |factor|
     if num % factor == 0
       other_factor = num / factor
-      prime_factorization(factor)
+      return [ *prime_factorization(factor), *prime_factorization(other_factor) ]
     end
   end
+
+  [ num ]
 end
+
+p prime_factorization(12)     # => [2, 2, 3]
+p prime_factorization(24)     # => [2, 2, 2, 3]
+p prime_factorization(25)     # => [5, 5]
+p prime_factorization(60)     # => [2, 2, 3, 5]
+p prime_factorization(7)      # => [7]
+p prime_factorization(11)     # => [11]
+p prime_factorization(2017) 
