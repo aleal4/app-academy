4b7fa269e0f2c56156b7e272dc8ca63d29afb636
Augusto <aleal4@gmail.com>
2022-04-24 21:58:51 -0700

    substring

--- Ruby/Enumerables & Debugging/enumerables/lib/enumerables_array.rb
+++ Ruby/Enumerables & Debugging/enumerables/lib/enumerables_array.rb
@@ -86,9 +86,19 @@ end
 # words).
 
 def substrings(string)
+  subs = []
+
+  (0...string.lenth - 1).each do |start_idx|
+    (start_idx...string.length).each do |end_idx|
+      subs << string[start_idx..end_idx]
+    end
+  end
+  subs
 end
 
 def subwords(word, dictionary)
+  subs = substrings(word)
+  subs.select {|sub| dictionary.include?(sub)}
 end
 
 # ### Doubler
