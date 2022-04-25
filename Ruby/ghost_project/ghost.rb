8f36ce857ab2adf51657e90d176a7caa007c3898
Augusto <aleal4@gmail.com>
2022-04-24 22:38:04 -0700

    dictionary added

--- Ruby/ghost_project/ghost.rb
+++ Ruby/ghost_project/ghost.rb
@@ -1,5 +1,12 @@
+require 'set'
+
 class GhostGame
-  
+  def initialize(*players)
+    words = File.readlines('dictionary.text').map(&:chomp)
+    @players = players
+    @fragment = ''
+    @dictionary = Set.new(words)
+  end
 
 
 end
