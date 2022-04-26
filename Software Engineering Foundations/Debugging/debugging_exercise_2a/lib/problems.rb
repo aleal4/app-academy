233d3f488050692160f5e6c2fda8d8f723ed58b2
Augusto <aleal4@gmail.com>
2022-04-26 16:30:15 -0700

    struggling

--- Software Engineering Foundations/Debugging/debugging_exercise_2a/lib/problems.rb
+++ Software Engineering Foundations/Debugging/debugging_exercise_2a/lib/problems.rb
@@ -1,12 +1,18 @@
 # Run `bundle exec rspec` and satisy the specs.
 # You should implement your methods in this file.
 # Feel free to use the debugger when you get stuck.
+require 'byebug'
 
 def prime(number)
   return false if number < 2
-  (0..number).none? {|i| number % i == 0}
+  (2...number).none? {|i| number % i == 0}
 end
 
 def largest_prime_factor(number)
-
+  debugger
+  number.downto(2) do |factor|
+    if number % factor == 0 && prime(number)
+      return factor
+    end
+  end
 end
