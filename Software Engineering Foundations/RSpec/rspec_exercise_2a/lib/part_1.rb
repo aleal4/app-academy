f77bc234237bf2a703091a8df4d8744dbffec78e
Augusto <aleal4@gmail.com>
2022-04-26 13:22:47 -0700

    partition

--- Software Engineering Foundations/RSpec/rspec_exercise_2a/lib/part_1.rb
+++ Software Engineering Foundations/RSpec/rspec_exercise_2a/lib/part_1.rb
@@ -1,3 +1,18 @@
 def partition(arr, n)
+  less = []
+  greater = []
+
+  arr.each do |ele|
+    if ele < n
+      less << ele
+    elsif ele >= n
+      greater << ele
+    end
+  end
+
+  [less, greater]
+end
+
+def merge(hash_1, hash_2)
 
 end
