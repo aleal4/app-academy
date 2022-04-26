a0e5c5ac3ed4fbf656b7b95eb21b77a188977d52
Augusto <aleal4@gmail.com>
2022-04-26 13:27:17 -0700

    new hashes

--- Software Engineering Foundations/RSpec/rspec_exercise_2a/lib/part_1.rb
+++ Software Engineering Foundations/RSpec/rspec_exercise_2a/lib/part_1.rb
@@ -14,5 +14,10 @@ def partition(arr, n)
 end
 
 def merge(hash_1, hash_2)
+  new_hash = {}
 
+  hash_1.each {|key, value| new_hash[key] = value }
+  hash_2.each {|key, value| new_hash[key] = value }
+
+  new_hash
 end
