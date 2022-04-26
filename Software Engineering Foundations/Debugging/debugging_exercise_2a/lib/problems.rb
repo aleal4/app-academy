5f6936903fcea54be777a7e05bbbac3ca0d68e21
Augusto <aleal4@gmail.com>
2022-04-26 16:22:42 -0700

    part 2 prime

--- Software Engineering Foundations/Debugging/debugging_exercise_2a/lib/problems.rb
+++ Software Engineering Foundations/Debugging/debugging_exercise_2a/lib/problems.rb
@@ -2,6 +2,11 @@
 # You should implement your methods in this file.
 # Feel free to use the debugger when you get stuck.
 
+def prime(number)
+  return false if number < 2
+  (0..number).none? {|i| number % i == 0}
+end
+
 def largest_prime_factor(number)
 
 end
