6db2ad51aead3c9ba5d3faa9a7d3687dc68822f9
Augusto <aleal4@gmail.com>
2022-04-26 23:34:16 -0700

    feeling groovy

--- Software Engineering Foundations/Blocks_Procs/procs_project1a/lib/part_2.rb
+++ Software Engineering Foundations/Blocks_Procs/procs_project1a/lib/part_2.rb
@@ -21,3 +21,28 @@ def greater_proc_value(num, prc_1, prc_2)
     return num_2
   end
 end
+
+def and_selector(array, prc_1, prc_2)
+  new_array = []
+
+  array.each do |ele|
+    if prc_1.call(ele) && prc_2.call(ele)
+      new_array << ele
+    end
+  end
+  new_array
+end
+
+def alternating_mapper(array, prc_1, prc_2)
+  new_array = []
+
+  array.each.with_index do |ele, idx|
+    if idx.even?
+      new_array << prc_1.call(ele)
+    elsif idx.odd?
+      new_array << prc_2.call(ele)
+    end
+  end
+
+  new_array
+end
