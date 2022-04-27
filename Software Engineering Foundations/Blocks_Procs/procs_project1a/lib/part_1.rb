07bedc1ef100836d854ca2166fd909c130709b3a
Augusto <aleal4@gmail.com>
2022-04-26 23:14:10 -0700

    procs

--- Software Engineering Foundations/Blocks_Procs/procs_project1a/lib/part_1.rb
+++ Software Engineering Foundations/Blocks_Procs/procs_project1a/lib/part_1.rb
@@ -3,3 +3,23 @@ def my_map(array, &prc)
   array.each {|ele| new_arr << prc.call(ele)}
   new_arr
 end
+
+def my_select(array, &prc)
+  new_arr = []
+
+  array.each do |ele| 
+    if prc.call(ele)
+      new_arr << ele
+    end
+  end
+  new_arr
+end
+
+def my_count(array, &prc)
+  count = 0
+
+  array.each do |ele|
+    count += 1 if prc.call(ele)
+  end
+  count
+end
