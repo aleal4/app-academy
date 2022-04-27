00f08a40f827484c1f293ba9d4eedefc90832b50
Augusto <aleal4@gmail.com>
2022-04-26 20:25:16 -0700

    positive_subarrays

--- Software Engineering Foundations/Blocks_Procs/blocks_project1a/lib/part_1.rb
+++ Software Engineering Foundations/Blocks_Procs/blocks_project1a/lib/part_1.rb
@@ -1,3 +1,11 @@
 def select_even_nums(array)
   array.select(&:even?)
 end
+
+def reject_puppies(array)
+  array.reject{|puppies| puppies['age'] <= 2}
+end
+
+def count_positive_subarrays(array)
+  array.count {|arr| arr.sum > 0 }
+end
