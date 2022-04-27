7790a4584551acc628ab8017589d364c8ac61be1
Augusto <aleal4@gmail.com>
2022-04-26 23:26:47 -0700

    procin

--- Software Engineering Foundations/Blocks_Procs/procs_project1a/lib/part_2.rb
+++ Software Engineering Foundations/Blocks_Procs/procs_project1a/lib/part_2.rb
@@ -2,3 +2,11 @@ def reverser(str, &prc)
   prc.call(str.reverse)
 end
 
+def word_changer(str, &prc)
+  words = str.split(' ')
+  new_words = []
+
+  words.each {|word| new_words << prc.call(word) }
+
+  new_words.join(' ')
+end
