b41ff690cb0344ee6739affabd950ea41ba4fb89
Augusto <aleal4@gmail.com>
2022-04-26 20:41:43 -0700

    passing students

--- Software Engineering Foundations/Blocks_Procs/blocks_project1a/lib/part_2.rb
+++ Software Engineering Foundations/Blocks_Procs/blocks_project1a/lib/part_2.rb
@@ -11,3 +11,11 @@ def no_valid_url?(array)
     end
   end
 end
+
+def any_passing_students?(array)
+  array.any? do |student| 
+    grades = student[:grades]
+    avg = grades.sum / (grades.length * 1.0)
+    avg >=75
+  end
+end
