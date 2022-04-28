507147059046e4cd9ce40619bf168a0ca4be66e5
Augusto <aleal4@gmail.com>
2022-04-28 06:21:45 -0700

    room class completed

--- Software Engineering Foundations/Input_Output/hotel_project 2/lib/room.rb
+++ Software Engineering Foundations/Input_Output/hotel_project 2/lib/room.rb
@@ -13,9 +13,22 @@ class Room
   end
 
   def full?
-    if @occupants.length < capacity
+    if @occupants.length < @capacity
       return false
     end
     true
+  end
+
+  def available_space
+    @capacity - @occupants.length
+  end
+
+  def add_occupant(occupant)
+    if self.full?
+      return false
+    else
+      @occupants << occupant
+      return true
+    end
   end
 end
