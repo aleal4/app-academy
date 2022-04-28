f498c5a3109a43de3cc8f987876d336247c05bba
Augusto <aleal4@gmail.com>
2022-04-28 08:28:43 -0700

    checking

--- Software Engineering Foundations/Input_Output/hotel_project 2/lib/hotel.rb
+++ Software Engineering Foundations/Input_Output/hotel_project 2/lib/hotel.rb
@@ -1,4 +1,4 @@
-require 'byebug'
+# require 'byebug'
 require_relative "room"
 
 class Hotel
@@ -11,8 +11,8 @@ class Hotel
     end
   end
 
-  def name 
-    @name.split(" ").map(&:capitalize).join(" ")
+  def name
+    p @name.split(" ").map(&:capitalize).join(" ")
   end
 
   def rooms
@@ -24,31 +24,6 @@ class Hotel
   end
 
   def check_in(person, room_name)
-    unless self.room_exists?(room_name)
-      puts 'sorry room doesn\'t exist'
-      return 
-    end
-
-    succesfully_added_occupant = @rooms[room_name].add_occupant(person)
-
-    if succesfully_added_occupant
-      puts 'check in successful'
-    else
-      puts 'sorry, room is already full'
-    end
-  end
-
-  def has_vacancy?
-    debugger
-    @rooms.values.any? do |room| 
-      room.available_space > 0
-    end
-  end
-
-  def list_rooms
-    @rooms.each do |room_name, room| 
-      puts "#{room_name} : #{room.available_space}"
-    end
+    
   end
 end
-
