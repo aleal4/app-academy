ce075c02eb32c93bd9aa9fab6a52bf4f5f4f775e
Augusto <aleal4@gmail.com>
2022-04-28 06:18:45 -0700

    room instance

--- Software Engineering Foundations/Input_Output/hotel_project 2/lib/room.rb
+++ Software Engineering Foundations/Input_Output/hotel_project 2/lib/room.rb
@@ -1,3 +1,21 @@
 class Room
-  
+  def initialize(capacity)
+    @capacity = capacity
+    @occupants = []
+  end
+
+  def capacity
+    @capacity
+  end
+
+  def occupants
+    @occupants
+  end
+
+  def full?
+    if @occupants.length < capacity
+      return false
+    end
+    true
+  end
 end
