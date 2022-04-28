e951829291bff5852ea3e62a4a91004e1e2e8884
Augusto <aleal4@gmail.com>
2022-04-27 18:22:26 -0700

    monkey around

--- Software Engineering Foundations/Monkey_Patching/monkey_patching_project 2/lib/array.rb
+++ Software Engineering Foundations/Monkey_Patching/monkey_patching_project 2/lib/array.rb
@@ -1,4 +1,16 @@
 # Monkey-Patch Ruby's existing Array class to add your own custom methods
 class Array
-  
+  def span
+    return nil if self.length == 0
+    return self.max - self.min
+  end
+
+  def average
+    return nil if self.empty?
+    return self.sum / (self.length * 1.0)
+  end
+
+  def median
+
+  end
 end
