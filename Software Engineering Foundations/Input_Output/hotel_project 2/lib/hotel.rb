569aeea822e5d28b1420ec15a0e2838b27731e00
Augusto <aleal4@gmail.com>
2022-04-28 08:46:55 -0700

    printing

--- Software Engineering Foundations/Input_Output/hotel_project 2/lib/hotel.rb
+++ Software Engineering Foundations/Input_Output/hotel_project 2/lib/hotel.rb
@@ -24,6 +24,26 @@ class Hotel
   end
 
   def check_in(person, room_name)
-    
+    unless self.room_exists?(room_name)
+      p 'sorry, room doesn\'t exist'
+      return
+    end
+
+    check_in_true_false = @rooms[room_name].add_occupant(person)
+    if check_in_true_false
+      p 'check in successful'
+    else
+      p 'sorry, room is full'
+    end
+  end
+
+  def has_vacancy?
+    @rooms.values.any? {|room| room.available_space > 0}
+  end
+
+  def list_rooms
+    @rooms.each do |room_name, room| 
+      puts "#{room_name} : #{room.available_space}"
+    end
   end
 end
