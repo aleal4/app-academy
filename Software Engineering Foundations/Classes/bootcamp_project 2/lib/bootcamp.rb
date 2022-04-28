17965513443cd1da56e0498759fa6218724187d5
Augusto <aleal4@gmail.com>
2022-04-27 17:37:40 -0700

    class initialized

--- Software Engineering Foundations/Classes/bootcamp_project 2/lib/bootcamp.rb
+++ Software Engineering Foundations/Classes/bootcamp_project 2/lib/bootcamp.rb
@@ -1,3 +1,10 @@
 class Bootcamp
-  
+  def initialize(name, slogan, student_capacity)
+    @name = name
+    @slogan = slogan
+    @student_capacity = student_capacity
+    @teachers = []
+    @students = []
+    @grades = Hash.new {|h,k| h[k] = [] }
+  end
 end
